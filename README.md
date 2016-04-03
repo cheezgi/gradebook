a0e1453f08310518b55ed55bf9cbc3b1115a39c8
Zack H <zackh@firemail.cc>
2016-04-03 16:30:20 +0000

    rename test.py, update install script

--- README.md
+++ README.md
@@ -3,16 +3,17 @@ flask/gunicorn web gradebook
 
 ## installation
 
-Run the following command to install `gradebook`:
+Run the following commands to install `gradebook`:
 
 ```
 $ git clone https://github.com/cheezgi/gradebook.git somepath
+# ./install.sh somepath
 ```
 
 This program relies on Flask and Gunicorn. To run as a Gunicorn app, run the following:
 
 ```
-# gunicorn -b 0.0.0.0:80 test:app
+# gunicorn -b 0.0.0.0:80 grades:app
 ```
 
 To deploy with a different server, you can uncomment the uWSGI related lines in `test.py`
--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 echo "Installing python-virtualenv"
 echo "----------------------------"
-yes | sudo apt install python-virtualenv
+yes |  apt install python-virtualenv
 if [ -n $1 ]
 then
     echo "Starting python virtual environment in $1"
     echo "-----------------------------------------"
     virtualenv $1
     $1/bin/activate
-    sudo $1/bin/pip install flask
-    sudo $1/bin/pip install wtforms
-    sudo $1/bin/pip install passlib
-    sudo $1/bin/pip install gunicorn
+    $1/bin/pip install flask
+    $1/bin/pip install wtforms
+    $1/bin/pip install passlib
+    $1/bin/pip install gunicorn
 else
     echo "Starting python virtual environment in grades"
     echo "---------------------------------------------"
     virtualenv grades
     grades/bin/activate
-    sudo grades/bin/pip install flask
-    sudo grades/bin/pip install wtforms
-    sudo grades/bin/pip install passlib
-    sudo grades/bin/pip install gunicorn
+    grades/bin/pip install flask
+    grades/bin/pip install wtforms
+    grades/bin/pip install passlib
+    grades/bin/pip install gunicorn
 fi
